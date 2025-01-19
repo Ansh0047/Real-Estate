8da9001b0e6f22586e29d9b6fb18e1e48bcce8c8
Ansh <anshkant090@gmail.com>
2025-01-19 19:18:06 +0530

    signin functionally done

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,73 @@
-import React from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    })
+  };
+
+  // console.log(formData);
+  // console.log(error);
+  
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();      // to avoid the refresh of the page when form is submitted
+      setLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message)
+    }
+  };
+
   return (
-    <div>SignUp</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="text" placeholder="username"
+          className='border p-3 rounded-lg' id='username' onChange={handleChange} />
+        <input type="text" placeholder="email"
+          className='border p-3 rounded-lg' id='email' onChange={handleChange} />
+        <input type="password" placeholder="password"
+          className='border p-3 rounded-lg' id='password' onChange={handleChange} />
+
+        {/* when loding just disable the button */}
+        <button disabled={loading} className='bg-slate-700 text-white p-3 
+        rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p> Have an account? </p>
+        <Link to="/signin">
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {/* {error && <p className='text-red-500 mt-5'>Enter Valid Credentials</p>} */}
+    </div>
   )
 }
 
