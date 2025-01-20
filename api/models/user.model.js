f80d16b8d367e0d3211a38d2232056eada966a28
Ansh <anshkant090@gmail.com>
2025-01-21 02:15:16 +0530

    oauth added

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,6 +17,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+      default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Fblank-profile-picture-mystery-man-973460%2F&psig=AOvVaw2AexPAb3aCYik0jrxNA2Xl&ust=1737491634770000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCOCms_KShYsDFQAAAAAdAAAAABAE",
+    },
   },
   { timestamps: true }
 );
--- client/src/component/OAuth.jsx
+++ client/src/component/OAuth.jsx
@@ -1,25 +1,45 @@
-import {GoogleAuthProvider, signInWithPopup, getAuth} from '@firebase/auth';
-import {app} from "../firebase";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from '@firebase/auth';
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import {useNavigate} from "react-router-dom";
 
 const OAuth = () => {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
     const handleGoogleClick = async () => {
         try {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app);
 
-            const result = await signInWithPopup(auth,provider);
-            console.log(result);
+            const result = await signInWithPopup(auth, provider);
+            // console.log(result);
+            // sending the necessary information to the backend, got from the google authentication
+            const res = await fetch('/api/auth/google', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    photo: result.user.photoURL,
+                }),
+            });
+            const data = await res.json();
+            dispatch(signInSuccess(data));
+            navigate("/");
         } catch (error) {
             console.log("can't connect to google", error);
         }
     };
 
-  return (
-    <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 
+    return (
+        <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 
     rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-        Continue With Google
-    </button>
-  );
+            Continue With Google
+        </button>
+    );
 };
 
 export default OAuth;
