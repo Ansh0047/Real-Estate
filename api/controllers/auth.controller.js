4c0910e09085034cb882b8fa2ad5260b7933935b
Ansh <anshkant090@gmail.com>
2025-01-20 15:42:01 +0530

    created signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req,res,next) => {
     // console.log(req.body);
@@ -24,4 +25,28 @@ export const signup = async (req,res,next) => {
         // next(errorHandler(550,"error from the function"));      // manually created error - custom error handler 
     }
     
+};
+
+export const signin = async (req,res,next) => {
+    const {email, password} = req.body;
+    try {
+        // to check if the user is valid by checking email in DB
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"User not found!"));
+        }
+        // else if email is valid then check password
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(404,"Invalid Credentials!"));
+        } 
+
+        // here we have destuctured the password and then send the data without pass
+        const {password: pass, ...rest} = validUser._doc;
+        // to store the session once logged in
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+        res.cookie("access_token", token,{httpOnly: true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 };
