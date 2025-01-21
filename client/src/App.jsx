f196cfd7b077e4be1227d683d4abb12f549b521a
Ansh <anshkant090@gmail.com>
2025-01-21 22:05:59 +0530

    made the profile page private using PrivateRoute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Header from "./component/Header";
+import PrivateRoute from "./component/PrivateRoute";
 
 
 function App() {
@@ -16,7 +17,10 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+        
       </Routes>
     </BrowserRouter>
   )
