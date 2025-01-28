9132194856c4cc341b82c06b3f49479053257b10
Ansh <anshkant090@gmail.com>
2025-01-28 16:40:23 +0530

    search api route created

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -59,3 +59,55 @@ export const getListing = async (req,res,next) => {
         next(error);
     }
 };
+
+
+// searching the items in the database
+export const getListings = async (req,res,next)=>{
+    try {
+        const limit = parseInt(req.query.limit) || 9;
+        const startIndex = parseInt(req.query.startIndex) || 0;
+
+        let offer = req.query.offer;
+        if (offer === undefined || offer === 'false') {
+            // this means if there is no choice selected then show all with both offer and no offer
+            offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = { $in: [false, true] };
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = { $in: [false, true] };
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+
+        const sort = req.query.sort || 'createdAt';
+
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            // to find the search term in the headings without considering the upper or lower case
+            name: { $regex: searchTerm, $options: 'i' },
+            offer,
+            furnished,
+            parking,
+            type,
+        })
+        .sort({ [sort]: order })
+        .limit(limit)
+        .skip(startIndex);
+
+        return res.status(200).json(listings);
+    } catch (error) {
+        next(error);
+    }
+}
